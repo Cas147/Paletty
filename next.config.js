f14b704d105cf05e33dadcf27b5768c4d02786d9
Cas147 <andrescas1948@gmail.com>
2023-04-13 22:27:23 -0500

    vercel variables

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {  NEXT_PUBLIC_OPENAI_API_KEY: process.env.NEXT_PUBLIC_OPENAI_API_KEY  }
 }
 
 module.exports = nextConfig
