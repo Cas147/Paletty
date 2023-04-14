0ea37362e64ab528d7c0cb7df47fd45011fe327a
Cas147 <andrescas1948@gmail.com>
2023-04-13 22:16:30 -0500

    vercel variables

--- src/pages/api/generate.ts
+++ src/pages/api/generate.ts
@@ -1,7 +1,7 @@
 import { ChatCompletionRequestMessageRoleEnum, Configuration, OpenAIApi } from 'openai'
 
 const configuration = new Configuration({
-  apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
+  apiKey: process.env.CLIENT_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
